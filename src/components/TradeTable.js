a660f5654e755907cde8f69e12c89e47d0c7d0fe
vincecoscia <xvincentcosciax@gmail.com>
2023-01-20 18:00:30 -0500

    Added some placeholder stuff

--- src/components/TradeTable.js
+++ src/components/TradeTable.js
@@ -25,13 +25,13 @@ const TradeTable = ({ trades }) => {
         {trades.map((trade) => (
           <div
             key={trade.id}
-            className="flex w-full flex-row items-center border-b border-gray-700  bg-gray-800 px-2 py-4 last:rounded-b-lg last:border-0 hover:bg-primary hover:bg-opacity-10 dark:text-white"
+            className="flex w-full flex-row items-center border-b border-gray-700  bg-gray-800 px-2 py-3 last:rounded-b-lg last:border-0 hover:bg-primary hover:bg-opacity-10 dark:text-white"
           >
             <div className="flex-1 text-primary">{trade.symbol}</div>
-            <div className="flex-1">{trade.dateOpened.toLocaleString()}</div>
-            <div className="flex-1">{trade.dateClosed.toLocaleString()}</div>
-            <div className="flex-1">{trade.openPrice}</div>
-            <div className="flex-1">{trade.closePrice}</div>
+            <div className="flex-1 font-light">{trade.dateOpened.toLocaleString()}</div>
+            <div className="flex-1 font-light">{trade.dateClosed.toLocaleString()}</div>
+            <div className="flex-1 font-light">{trade.openPrice}</div>
+            <div className="flex-1 font-light">{trade.closePrice}</div>
             <div
               className={
                 trade.netProfit > 0
--- src/pages/dashboard/overview.tsx
+++ src/pages/dashboard/overview.tsx
@@ -11,6 +11,8 @@ import TradeTable from "../../components/TradeTable";
 
 const Dashboard: NextPage = () => {
   const [trades, setTrades] = useState<Trade[]>([]);
+  const [executions, setExecutions] = useState<Execution[]>([]);
+  const [balance, setBalance] = useState<number>(0);
   const { data: sessionData } = useSession();
 
   const { data: tradeData, isLoading } = trpc.tradeRouter.getTrades.useQuery(
@@ -18,18 +20,24 @@ const Dashboard: NextPage = () => {
     {
       onSuccess(tradeData) {
         setTrades(tradeData);
-        console.log(tradeData)
+        console.log(tradeData);
       },
     }
   );
 
-  if (!tradeData || isLoading) {
-    return (
-      <>
-        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-      </>
-    );
-  }
+  const { data: executionData, isLoading: executionLoading } =
+    trpc.executionRouter.getExecutions.useQuery(undefined, {
+      onSuccess(executionData) {
+        setExecutions(executionData);
+      },
+    });
+
+    // TODO: Add a loading state
+
+  // Get account returns by summing all trade.netProfit
+  const accountReturns = trades.reduce((acc, trade) => {
+    return acc + trade.netProfit;
+  }, 0);
 
   return (
     <>
@@ -44,7 +52,25 @@ const Dashboard: NextPage = () => {
       <main className="flex h-[calc(100vh-84px)] bg-white dark:bg-gray-900">
         <SideNav />
         <div className="m-4 w-full">
-          {/* Map over trades */}
+          <div className="mb-4 grid grid-cols-3 gap-4">
+            <div className="col-span-2 flex flex-col">
+              <div className="mb-4 flex justify-between">
+                <p className="rounded-lg p-2 font-light dark:bg-gray-800 dark:text-white">
+                  Total: {trades.length} (Trades) | Return: <span className={accountReturns > 0 ? 'text-green-500' : 'text-red-500'}>${accountReturns.toFixed(2)}</span>
+                </p>
+                <button className="w-20 rounded-lg bg-gray-800 p-2 text-white">
+                  All
+                </button>
+              </div>
+              <div className="col-span-2 flex h-64 rounded-lg bg-gray-800 p-2 text-white">
+                Chart
+              </div>
+            </div>
+
+            <div className="flex h-full rounded-lg bg-gray-800 p-2 text-white">
+              Test
+            </div>
+          </div>
           <TradeTable trades={trades} />
         </div>
       </main>
