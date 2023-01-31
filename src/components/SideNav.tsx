3bc677d4efcea983626fa298cdefb4015b34c6ba
vincecoscia <xvincentcosciax@gmail.com>
2023-01-30 19:13:15 -0500

    Added Journal to sideNav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -28,104 +28,135 @@ export default function SideNav() {
   };
 
   return (
-    <aside className="h-full w-64 flex-none hidden lg:block" aria-label="Sidenav">
-      <div className="h-full overflow-y-auto bg-white py-5 px-3 dark:border-gray-700 dark:bg-gray-800 flex flex-col justify-between">
+    <aside
+      className="hidden h-full w-64 flex-none lg:block"
+      aria-label="Sidenav"
+    >
+      <div className="flex h-full flex-col justify-between overflow-y-auto bg-white py-5 px-3 dark:border-gray-700 dark:bg-gray-800">
         <div>
-        <ul className="space-y-2">
-          <li>
-            <Link
-              href="/dashboard"
-              className={
-                "group flex items-center rounded-lg p-2 text-base font-normal transition duration-75 " +
-                isActiveLink("/dashboard")
-              }
-            >
-              <svg
-                aria-hidden="true"
+          <ul className="space-y-2">
+            <li>
+              <Link
+                href="/dashboard"
                 className={
-                  "h-6 w-6 transition duration-75" +
-                  isActiveSVG("/dashboard")
-                }
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-              </svg>
-              <span className="ml-3">Overview</span>
-            </Link>
-          </li>
-        </ul>
-        <ul className="mt-5 space-y-2 border-t border-gray-200 pt-5 dark:border-gray-700">
-          <li>
-            <Link
-              href="/dashboard/import"
-              className={
-                "group flex items-center rounded-lg p-2 text-base font-normal transition duration-75 " +
-                isActiveLink("/dashboard/import")
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className={
-                  "h-6 w-6 transition duration-75" +
-                  isActiveSVG("/dashboard/import")
+                  "group flex items-center rounded-lg p-2 text-base font-normal transition duration-75 " +
+                  isActiveLink("/dashboard")
                 }
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
-                />
-              </svg>
+                <svg
+                  aria-hidden="true"
+                  className={
+                    "h-6 w-6 transition duration-75" + isActiveSVG("/dashboard")
+                  }
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                </svg>
+                <span className="ml-3">Overview</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/trades"
+                className={
+                  "group flex items-center rounded-lg p-2 text-base font-normal transition duration-75 " +
+                  isActiveLink("/dashboard/trades")
+                }
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={
+                    "h-6 w-6 transition duration-75" +
+                    isActiveSVG("/dashboard/trades")
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                  />
+                </svg>
 
-              <span className="ml-3">Import Trades</span>
-            </Link>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              <svg
-                aria-hidden="true"
-                className="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+                <span className="ml-3">Journal</span>
+              </Link>
+            </li>
+          </ul>
+          <ul className="mt-5 space-y-2 border-t border-gray-200 pt-5 dark:border-gray-700">
+            <li>
+              <Link
+                href="/dashboard/import"
+                className={
+                  "group flex items-center rounded-lg p-2 text-base font-normal transition duration-75 " +
+                  isActiveLink("/dashboard/import")
+                }
               >
-                <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
-              </svg>
-              <span className="ml-3">Components</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              <svg
-                aria-hidden="true"
-                className="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={
+                    "h-6 w-6 transition duration-75" +
+                    isActiveSVG("/dashboard/import")
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+                  />
+                </svg>
+
+                <span className="ml-3">Import Trades</span>
+              </Link>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="ml-3">Help</span>
-            </a>
-          </li>
-        </ul>
+                <svg
+                  aria-hidden="true"
+                  className="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
+                </svg>
+                <span className="ml-3">Components</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="ml-3">Help</span>
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="flex justify-center space-x-4 bg-white p-4 dark:bg-gray-800">
           <a
