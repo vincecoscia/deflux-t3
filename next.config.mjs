ca62da7540c25ab369c865664b2e384fffe58dca
vincecoscia <xvincentcosciax@gmail.com>
2023-02-15 23:14:27 -0500

    Added deflux to config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["images.unsplash.com", "lh3.googleusercontent.com", "deflux-test.s3.amazonaws.com", "deflux-qa.s3.amazonaws.com"],
+    domains: ["images.unsplash.com", "lh3.googleusercontent.com", "deflux-test.s3.amazonaws.com", "deflux-qa.s3.amazonaws.com", "deflux.io"],
   },
 };
 export default config;
